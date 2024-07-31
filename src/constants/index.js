8f5809b1599c9ae3b191cb8c1a745e178eec9b75
Karthikeyan <karthikeyanpalanisamy1999@gmail.com>
2024-07-31 19:20:41 +0530

    updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,9 @@ const { APP_VERSION } = require('./appVersion.constants')
 
 const { STATUS_CODES } = require('./statusCode.constants')
 
-const {PAYMENT_METHOD} = require('./user.constants')
 
 
 module.exports = {
     APP_VERSION,
     STATUS_CODES, 
-    PAYMENT_METHOD
 }
