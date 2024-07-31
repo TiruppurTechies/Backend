b0d09ac0262d4aa8b78a9b0cb17f819c0b120002
Karthikeyanpalanisamy1999 <154215415+Karthikeyanpalanisamy1999@users.noreply.github.com>
2024-07-31 13:36:28 +0530

    Feat: Add swagger Documents (#6)

--- src/routes/userData.js
+++ src/routes/userData.js
@@ -2,7 +2,48 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/userData')
 
-router.post('/',controller.saveUserData)
+/**
+ * @swagger
+ *  tags:
+ *    name: UserData
+ *    description: API Endpoints for managing UserData
+ */
+
+/**
+ * @swagger
+ * /userData:
+ *   post:
+ *     summary: Save the UserData 
+ *     description: Post the userData to the database 
+ *     tags: [UserData]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userName:
+ *                 type: string
+ *                 description: Name of the user.
+ *               mobile:
+ *                 type: string
+ *                 description: Mobile number of the user.
+ *               password:
+ *                 type: string
+ *                 description: Password created by the user.
+ *               confirmPassword:
+ *                 type: string
+ *                 description: It should be equal to the password.
+ *     responses:
+ *       200:
+ *         description: UserData saved successfully
+ *       400:
+ *         description: Bad request error
+ *       500:
+ *         description: Internal server error
+ */
+
+router.post('/', controller.saveUserData)
 
 module.exports = router
-
